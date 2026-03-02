2ea84a4ba2cc9fa92f3544b49f8e8219eadb6434
luluxiaoliao-cloud <luluxiaoliao@gmail>
2026-03-02 22:49:08 +0800

    Add basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/myportofolio',
+  assetPrefix: '/myportofolio',
   images: {
     unoptimized: true,
   },
